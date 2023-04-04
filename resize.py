8351235e6f97d14302283600c4f5019da9515d35
GodNoden <noe.quezada7446@alumnos.udg.mx>
2023-04-04 11:18:35 -0600

    deleted d letter, making tests

--- resize.py
+++ resize.py
@@ -3,7 +3,7 @@ import cv2 as cv
 
 def rescaleFrame(frame, scale=0.75):
     width = int(frame.shape[1] * scale)
-    height = int(dframe.shape[0] * scale)
+    height = int(frame.shape[0] * scale)
     dimensions = (width, height)
 
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
